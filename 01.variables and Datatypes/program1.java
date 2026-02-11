094b41905bebcc3225bfd93732b7177d9580394b
Hamsini S <hamsinis023@gmail.com>
2026-02-11 22:54:28 +0530

    Add files via upload

---
+++ 01.variables and Datatypes/program1.java
@@ -0,0 +1,9 @@
+//! 1.Greatest of three number
+
+public class program1 {
+    public static void main(String[] args) {
+       int a= 10,b=20,c=30;
+       int res = a>b?(a>c?a:c):(b>c?b:c); 
+       System.out.println(res);
+    }
+}
