21de69ab373f00dc6c44f56fb0340eb4ddb5609b
Hamsini S <hamsinis023@gmail.com>
2026-02-12 18:37:33 +0530

    Add files via upload

---
+++ 01.variables and Datatypes/program4.java
@@ -0,0 +1,10 @@
+//!1. widening primitive typecasting
+public class program4 {
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
