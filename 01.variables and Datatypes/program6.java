e0e4994b7a36df34dc6c6fe342a4643ba90ecd40
Hamsini S <hamsinis023@gmail.com>
2026-02-12 18:38:14 +0530

    Add files via upload

---
+++ 01.variables and Datatypes/program6.java
@@ -0,0 +1,16 @@
+//!Number to charecter conversion 
+public class program6 {
+    public static void main(String[] args) {
+        char a = 'A';
+        int num1 = a;
+        System.out.println(a);
+        System.out.println(num1);
+        System.out.println("&&&&&&&&&&&&");
+        char num2 = 97;
+        int b = num2;
+        System.out.println(num2);
+        System.out.println(b);
+
+
+    }
+}
