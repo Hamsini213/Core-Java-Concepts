0935839fc9d19bf3332788713bc510da443385b8
Hamsini S <hamsinis023@gmail.com>
2026-02-12 18:37:53 +0530

    Add files via upload

---
+++ 01.variables and Datatypes/program5.java
@@ -0,0 +1,9 @@
+//! 2.Narrowing primitive typecasting(forcefully using cast operator)
+public class program5 {
+    public static void main(String[] args) {
+        double d = 500000;
+        int a =(int) d;
+        System.out.println(d);
+        System.out.println(a);
+    }
+}
