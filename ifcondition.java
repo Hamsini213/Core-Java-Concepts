5e9ee80b5e8b9a1b372bfe75ba8abbfa6cd5c5ff
Hamsini S <hamsinis023@gmail.com>
2026-02-13 21:59:07 +0530

    Add files via upload

---
+++ ifcondition.java
@@ -0,0 +1,10 @@
+//! 1. If Statement
+// todo----- Write a program to check weather number is even---------//
+public class ifcondition {
+    public static void main(String[] args) {
+        int a =20;
+        if (a%2==0) {
+            System.out.println(a+" is even number");
+        }
+    }
+}
