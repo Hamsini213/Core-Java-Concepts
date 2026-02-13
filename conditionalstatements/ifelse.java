043b4129d4c2660ce99fa0725bb50af638447f42
Hamsini S <hamsinis023@gmail.com>
2026-02-13 22:01:39 +0530

    Add files via upload

---
+++ conditionalstatements/ifelse.java
@@ -0,0 +1,12 @@
+//! 2.If Else
+//todo--------------check weather number is even or odd----------//
+public class ifelse {
+    public static void main(String[] args) {
+        int n = 10;
+        if (n%2==0) {
+            System.out.println(n+"even no");
+        } else {
+            System.out.println(n+"odd no");
+        }
+    }
+}
