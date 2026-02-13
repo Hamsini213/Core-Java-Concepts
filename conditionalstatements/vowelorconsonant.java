6bb2642f3df1e0a28a5c291d804f38829c016a5a
Hamsini S <hamsinis023@gmail.com>
2026-02-13 22:03:10 +0530

    Add files via upload

---
+++ conditionalstatements/vowelorconsonant.java
@@ -0,0 +1,20 @@
+public class vowelorconsonant {
+    public static void main(String[] args) {
+        char selector = 'h';
+        switch (selector) {
+            case 'a':
+            case 'e':
+                case 'i':
+                    case 'o':
+                        case 'u':{
+                            System.out.println("is vowel");
+                            break;
+                        }
+            default:{
+                System.out.println("is consonent");
+            }
+                break;
+        }
+    }
+    
+}
