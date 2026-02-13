b0d993f065134a39c05f65c3e02f4ec95bee0aa0
Hamsini S <hamsinis023@gmail.com>
2026-02-13 22:02:02 +0530

    Add files via upload

---
+++ conditionalstatements/elseifladder.java
@@ -0,0 +1,20 @@
+// ! elseif ladder
+// greatest of three numbers
+public class elseifladder {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int c = 30;
+        if (a<b)
+             {
+            System.out.println(a+"is greater number"); 
+             }
+        else if (b>c) {
+            System.out.println(b+"is grater number");
+        }
+        else{
+            System.out.println(c+"is greater number");
+        }
+
+        }
+    }
