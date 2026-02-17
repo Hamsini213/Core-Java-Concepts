e4ce21c422b654dd9b67c1a8b5ea2364f8402617
Hamsini S <hamsinis023@gmail.com>
2026-02-17 22:56:50 +0530

    Add files via upload

---
+++ 04.Methos or Functions/StaticMethod.java
@@ -0,0 +1,12 @@
+//! Static Method
+package methods;
+
+public class StaticMethod {
+    static void test()
+    {
+        System.out.println("From test-1");
+    }
+    public static void main(String[] args) {
+      test();
+    }
+}
