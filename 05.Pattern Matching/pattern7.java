9c585ea8e13292af81b62fd54e94cf97e9b2b6c3
Hamsini S <hamsinis023@gmail.com>
2026-02-18 23:01:08 +0530

    Add files via upload

---
+++ 05.Pattern Matching/pattern7.java
@@ -0,0 +1,22 @@
+ /*
+Pattern-7
+EEEEE
+DDDDD
+CCCCC
+BBBBB
+AAAAA
+ */
+public class pattern7{
+public static void main(String[] args) {
+	int row = 5;
+	int col = 5;
+	char ch = 'E';
+	for(int i = 0;i< row;i++) {
+		for(int j =0;j<col;j++) {
+			System.out.print(ch);
+		}
+		System.out.println();
+		ch--;
+	}
+}
+}
