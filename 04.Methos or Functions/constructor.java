52a20a290ac706c85999382c985d08e0a916642a
Hamsini S <hamsinis023@gmail.com>
2026-02-19 22:36:04 +0530

    Add files via upload

---
+++ 04.Methos or Functions/constructor.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class constructor {
+    public constructor()
+    {
+        System.out.println("From constructor");
+    }
+    public static void main(String[] args) {
+       constructor Object1 = new constructor();
+       constructor object2 = new constructor();
+       constructor object3= new constructor();  
+    }
+    
+}
