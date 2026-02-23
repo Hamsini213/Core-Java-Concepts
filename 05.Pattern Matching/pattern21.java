ec2926bc88298753733ff763151d950b66af16a6
Hamsini S <hamsinis023@gmail.com>
2026-02-23 22:15:53 +0530

    Add files via upload

---
+++ 05.Pattern Matching/pattern21.java
@@ -0,0 +1,36 @@
+
+    /*
+Pattern-21
+E    
+DD   
+CCC  
+BBBB 
+AAAAA
+
+Note that for diagonal i = j
+for lower triangle i>j(i greater than j for lower)
+for upper triangle i<j(i lesser than j for upper)
+ */
+
+public class pattern21 {
+	public static void main(String[] args) {
+		int row = 5;
+		int col = 5;
+		char ch = 'E';
+		for(int i = 0;i< row;i++) {
+			for(int j = 0;j< col;j++) {
+				if(i>=j) {
+					System.out.print(ch);
+				}
+				else {
+					System.out.print(' ');
+				}
+				
+			}
+			System.out.println();
+			ch--;
+		}
+	}
+
+}
+
