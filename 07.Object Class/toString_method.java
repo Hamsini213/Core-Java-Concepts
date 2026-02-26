a9a80be657bb49728a2439dee412e9a9d1f5c52c
Hamsini S <hamsinis023@gmail.com>
2026-02-26 22:26:46 +0530

    Add files via upload

---
+++ 07.Object Class/toString_method.java
@@ -0,0 +1,16 @@
+
+public class toString_method{
+
+    public static void main(String[] args) {
+        toString_method ts1= new toString_method();
+        toString_method ts2= new toString_method();
+        System.out.println(ts1);
+        System.out.println(ts2);
+        System.out.println("----------------");
+        System.out.println(ts1.toString());
+        System.out.println(ts2.toString());
+
+
+
+    }
+}
