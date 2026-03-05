12618f49d894688ee2cca5aa48c3cce0461874bb
Hamsini S <hamsinis023@gmail.com>
2026-03-05 22:06:04 +0530

    Add files via upload

---
+++ 08.simple Program/adding.java
@@ -0,0 +1,13 @@
+public class adding {
+ public static int add(int a, int b,int c) {
+	 int sum = a+b+c;
+	 return sum;
+	
+}
+ public static void main(String[] args) {
+	System.out.println(add(10,20,30));
+	
+
+}
+}
+
