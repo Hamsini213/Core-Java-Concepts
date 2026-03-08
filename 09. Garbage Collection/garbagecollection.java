466242a50ce457c0825b7271581d19741a714f15
Hamsini S <hamsinis023@gmail.com>
2026-03-08 21:10:13 +0530

    Add files via upload

---
+++ 09. Garbage Collection/garbagecollection.java
@@ -0,0 +1,13 @@
+//!Garbage Collection
+public class garbagecollection {
+
+	    int a = 100;
+	    int b = 200;
+	    public static void main(String[] args) {
+	    	garbagecollection d1 = new garbagecollection();
+	        System.out.println(d1);
+
+	        garbagecollection ob1 = new garbagecollection();
+	        System.out.println(d1);    
+	    }
+	}
