7382d68e32a014ba187ea6e216aff5dd2bb88b31
Hamsini S <hamsinis023@gmail.com>
2026-03-16 23:01:11 +0530

    Add files via upload

---
+++ 12.String/String_litrals.java
@@ -0,0 +1,13 @@
+ public class String_litrals {
+    public static void main(String[] args) {
+        String s1="java";
+        String s2="python";
+        String s3="Java";
+        String s4="java";
+        System.out.println(s1==s2);
+        System.out.println(s1==s3);
+        System.out.println(s1==s4);
+        System.out.println(s3==s1);
+        System.out.println(s3==s4);
+    }
+} 
