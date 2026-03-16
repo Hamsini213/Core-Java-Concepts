be7ab8d8771ced91c19077cd5912a0b15c8e2e02
Hamsini S <hamsinis023@gmail.com>
2026-03-16 23:02:52 +0530

    Add files via upload

---
+++ 12.String/String_method4 .java
@@ -0,0 +1,16 @@
+public class String_method4 {
+    public static void main(String[] args) {
+        String s = "Darshan";
+        String s1 = "";
+        String s2 = "krish";
+        System.out.println(s.isEmpty());// isempty
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+        System.out.println("-------------------------");
+        System.out.println(s.isBlank());//isblank
+        System.out.println(s1.isBlank());
+        System.out.println(s2.isBlank());
+    }
+    
+}
+
