0118032a2aad3704d0db699e30b739f64d5a07f0
Hamsini S <hamsinis023@gmail.com>
2026-03-16 23:03:32 +0530

    Add files via upload

---
+++ 12.String/String_method6.java
@@ -0,0 +1,14 @@
+
+    public class String_method6 {
+    public static void main(String[] args) {
+        String s = "java is easy language";
+        String s1 = "sql is query language";
+        String s2 = "WEB CONSIST OF HTML,CSS,JS";
+        String s3 = "JDBC IS A API";
+        System.out.println(s.toUpperCase());// toupper
+        System.out.println(s1.toUpperCase());
+        System.out.println("-------------------------");
+        System.out.println(s2.toLowerCase());//tolower
+        System.out.println(s3.toLowerCase());
+    }
+}
