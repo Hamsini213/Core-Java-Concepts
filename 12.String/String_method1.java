899dee3feffc7215e5f4c5de2470f97d3ff305cc
Hamsini S <hamsinis023@gmail.com>
2026-03-16 23:01:34 +0530

    Add files via upload

---
+++ 12.String/String_method1.java
@@ -0,0 +1,14 @@
+ public class String_method1 {
+    public static void main(String[] args) {
+        String s = "String";
+        String s1 = "java";
+        String s2 = "office word";
+        System.out.println(s.length());// length method
+        System.out.println(s1.length());
+        System.out.println(s2.length());
+        System.out.println("---------------------");
+        System.out.println(s.charAt(2));// charAt method
+        System.out.println(s1.charAt(3));
+        System.out.println(s2.charAt(7));
+    }
+}
