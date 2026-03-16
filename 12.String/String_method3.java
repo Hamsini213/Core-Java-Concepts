4fff72be6d760ae2a24c18247c292fe434e54f15
Hamsini S <hamsinis023@gmail.com>
2026-03-16 23:02:32 +0530

    Add files via upload

---
+++ 12.String/String_method3.java
@@ -0,0 +1,19 @@
+
+    public class String_method3 {
+    public static void main(String[] args) {
+        String s = "Darshan";
+        String s1 = "sudeep";
+        String s2 = "krish";
+        System.out.println(s.substring(2));// substring
+        System.out.println(s1.substring(4));
+        System.out.println(s2.substring(5));
+        System.out.println("-------------------------");
+        System.out.println(s.substring(0,4));
+        System.out.println(s1.substring(2,4));
+        System.out.println(s2.substring(0,4));
+
+
+    }
+}
+    
+}
