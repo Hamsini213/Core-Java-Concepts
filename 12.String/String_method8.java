650a71f65769365d005ba14d28a94795aeb0ad91
Hamsini S <hamsinis023@gmail.com>
2026-03-17 22:34:54 +0530

    Add files via upload

---
+++ 12.String/String_method8.java
@@ -0,0 +1,11 @@
+public class String_method8 {
+    public static void main(String[] args) {
+        String s = "java is easy language   ";
+        String s1="sql is query language";
+        System.out.println(s.indexOf("a"));// indexof
+        System.out.println(s1.indexOf("s"));
+        System.out.println("--------------------");
+         System.out.println(s.lastIndexOf("a"));//lastindexof
+        System.out.println(s1.lastIndexOf("s"));
+    }
+}
