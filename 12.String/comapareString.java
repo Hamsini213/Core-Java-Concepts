85fb1b5f5fcfaa01d265920246ff35a29bde9ca5
Hamsini S <hamsinis023@gmail.com>
2026-03-17 22:40:37 +0530

    Add files via upload

---
+++ 12.String/comapareString.java
@@ -0,0 +1,9 @@
+public class comapareString {
+    public static void main(String[] args) {
+        String s1="java";
+        String s2="python";
+        String s3="java";
+        System.out.println(s1==s3);
+        System.out.println(s2==s3);
+    }
+}
