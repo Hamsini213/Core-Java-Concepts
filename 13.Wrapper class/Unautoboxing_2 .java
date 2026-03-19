53485402d9b820bb041c7fcfe83b362e25252d7f
Hamsini S <hamsinis023@gmail.com>
2026-03-19 22:09:59 +0530

    Add files via upload

---
+++ 13.Wrapper class/Unautoboxing_2 .java
@@ -0,0 +1,10 @@
+
+    public class Unautoboxing_2 {
+     public static void main(String[] args) {
+     Integer num = new Integer(100);
+     int ob =num;
+     System.out.println(num);
+     System.out.println("----------------------");
+     System.out.println(ob);   
+    }
+}
