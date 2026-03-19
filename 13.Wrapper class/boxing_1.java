a7acc9fbd00e4005385b94ed77a3d4620b52bfad
Hamsini S <hamsinis023@gmail.com>
2026-03-19 22:06:30 +0530

    Add files via upload

---
+++ 13.Wrapper class/boxing_1.java
@@ -0,0 +1,16 @@
+
+    public class boxing_1 {
+    public static void main(String[] args) {
+      double d = 100.155d;
+      Double D = Double.valueOf(d);
+
+      short s = 10;
+      Short S = Short.valueOf(s);
+
+      System.out.println(d);
+      System.out.println(D);
+      System.out.println("-----------------------");
+      System.out.println(s);
+      System.out.println(S);
+    }
+}
