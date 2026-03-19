a91aad7cb19e628d42f2271c50e796e7cb8235c4
Hamsini S <hamsinis023@gmail.com>
2026-03-19 22:09:21 +0530

    Add files via upload

---
+++ 13.Wrapper class/boxing2.java
@@ -0,0 +1,17 @@
+
+public class boxing2 {
+    public static void main(String[] args) {
+      int i = 100;
+      Integer I = Integer.valueOf(i);
+
+      float f = 10.2f;
+      Float F = Float.valueOf(f);
+
+      System.out.println(i);
+      System.out.println(I);
+      System.out.println("-----------------------");
+      System.out.println(f);
+      System.out.println(F);
+    }
+}
+
