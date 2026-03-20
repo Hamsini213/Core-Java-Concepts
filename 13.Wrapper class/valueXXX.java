f27b0efcd4b463eb2da8a4a163816ddd367a9ad6
Hamsini S <hamsinis023@gmail.com>
2026-03-20 22:18:20 +0530

    Add files via upload

---
+++ 13.Wrapper class/valueXXX.java
@@ -0,0 +1,17 @@
+public class valueXXX {
+   
+     public static void main(String[] args) {
+      int i = 100;
+      Integer I = Integer.valueOf(i);
+
+      float f = 10.25f;
+      Float F = Float.valueOf(f);
+
+      System.out.println(i);
+      System.out.println(I);
+      System.out.println("-----------------------");
+      System.out.println(f);
+      System.out.println(F);
+    }
+}
+
