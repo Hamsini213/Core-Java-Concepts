158d2f4ab333a4e8ad5984762ff0607389938607
Hamsini S <hamsinis023@gmail.com>
2026-03-20 22:16:43 +0530

    Add files via upload

---
+++ 13.Wrapper class/Autoboxing_2.java
@@ -0,0 +1,10 @@
+public class Autoboxing_2 {
+    public static void main(String[] args) {
+        int num=100;
+        System.out.println(num);
+        test(num);
+    }
+    public static void test(Integer a){
+        System.out.println(a);
+    }
+}
