e4264acbe2ba3dfe93548ac5da99f9be20371870
Hamsini S <hamsinis023@gmail.com>
2026-03-20 22:19:12 +0530

    Add files via upload

---
+++ 13.Wrapper class/xxxvalue.java
@@ -0,0 +1,26 @@
+public class xxxvalue {
+    
+      public static void main(String[] args) {
+        Double d1 = new Double(102.f);
+        double D=d1.doubleValue();
+
+        Boolean b = new Boolean("true");
+        boolean B=b.booleanValue();
+
+        Double d = new Double(1001.1254d);
+        int i = d.intValue();
+        float f = d.floatValue();
+        double dd= d.doubleValue();
+        
+        System.out.println(d);
+        System.out.println(D);
+        System.out.println("-------------------");
+        System.out.println(b);
+        System.out.println(B);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(dd);
+
+    
+    } 
+}
