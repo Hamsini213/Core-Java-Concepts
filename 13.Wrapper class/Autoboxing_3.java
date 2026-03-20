64137947ea81d0b603617538825b99e4569831ca
Hamsini S <hamsinis023@gmail.com>
2026-03-20 22:17:57 +0530

    Add files via upload

---
+++ 13.Wrapper class/Autoboxing_3.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Autoboxing_3 {
+    public static void main(String[] args) {
+        ArrayList A = new ArrayList();
+        A.add("hamsini");
+        A.add("manish");
+        A.add("tanish");
+        System.out.println(A);
+    }
+} 
