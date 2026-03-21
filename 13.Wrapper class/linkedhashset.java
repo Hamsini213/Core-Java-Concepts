c688eed4581eeeff651e167b28fbef9db78a789b
Hamsini S <hamsinis023@gmail.com>
2026-03-21 22:44:42 +0530

    Add files via upload

---
+++ 13.Wrapper class/linkedhashset.java
@@ -0,0 +1,15 @@
+
+    import java.util.LinkedHashSet;
+
+public class linkedhashset {
+    public static void main(String[] args) {
+          LinkedHashSet h =new LinkedHashSet<>();
+          h.add("apple");
+          h.add("mango");
+          h.add("banana");
+          h.add("grape");
+          System.out.println(h);
+
+    }
+}
+
