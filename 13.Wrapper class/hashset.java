79b94639e965ddd00cd575b4ad375410f5cd0ed2
Hamsini S <hamsinis023@gmail.com>
2026-03-22 21:55:37 +0530

    Add files via upload

---
+++ 13.Wrapper class/hashset.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+public class hashset {
+    public static void main(String[] args) {
+         HashSet h=new HashSet<>();
+         h.add("java");
+         h.add("python");
+         h.add("jdbc");
+         System.out.println(h);
+    }
+}
